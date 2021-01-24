9d4bf4d4aae00da6770161cfdd82bd7df04a9b71
dak79 <danielecampari@gmail.com>
2021-01-25 01:51:10 +0300

    ver 0.5

--- js/script.js
+++ js/script.js
@@ -9,14 +9,103 @@ let sixLetters = ['anyone', 'artist', 'behalf', 'beyond', 'client', 'crisis', 'd
 let sevenLetters = ['advised', 'anybody', 'beneath', 'burning', 'chamber', 'chronic', 'display', 'dynamic', 'elderly', 'enhance', 'foreign', 'further', 'gateway', 'gigabit', 'hearing', 'himself', 'instant', 'involve', 'kitchen', 'knowing', 'learned', 'limited', 'massive', 'mention', 'neither', 'nuclear', 'outcome', 'organic', 'portion', 'payment', 'replace', 'routine', 'serious', 'similar', 'thought', 'typical', 'upgrade', 'utility', 'veteran', 'visible', 'wanting', 'welcome'];
 
 let eightLetters = ['achieved', 'anywhere', 'boundary', 'business', 'chemical', 'colorful', 'deadline', 'disclose', 'earnings', 'evidence', 'flagship', 'frontier', 'genomics', 'grateful', 'historic', 'humanity', 'initiate', 'isolated', 'judgment', 'junction', 'keyboard', 'laughter', 'literary', 'meantime', 'merchant', 'northern', 'numerous', 'optimism', 'overseas', 'patience', 'pipeline', 'regional', 'research', 'scrutiny', 'solution', 'taxpayer', 'tropical', 'unlawful', 'unlikely', 'weakness', 'withdraw', 'yourself'];
+let point = 0;
+let level = 0;
+let i;
+let rW;
+let w;
+let downloadTimer;
+let h;
 
-let timeleft = 10;
-let downloadTimer = setInterval(timer, 1000);
 
-function timer() {
-  if (timeleft <= 0) {
-    clearInterval(downloadTimer);
+
+window.addEventListener('keydown', start);
+
+function start(evt) {
+  if (evt.key === 'Enter') {
+    random();
+    countDown();
+    window.removeEventListener('keydown', start);
+    document.addEventListener('input', wordValue);
+
+    function wordValue() {
+      let word = document.querySelector('#typeWord');
+      let w2 = word.value;
+      if (w2 === rW) {
+        point += 100;
+        let addPoint = document.querySelector('#point');
+        addPoint.innerHTML = point;
+        i += 1;
+        level += 1;
+        let addLevel = document.querySelector('#level');
+        addLevel.innerHTML = level;
+        document.querySelector('#typeWord').value = "";
+        random();
+        countDown();
+      }
+    }
+
   }
-  document.querySelector("#progressBar").value = timeleft;
-  timeleft -= 1;
+}
+
+function countDown() {
+  let timeleft = 10;
+  let downloadTimer1 = setInterval(timer, 1000);
+
+  function timer() {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      console.log('hai perso');
+    }
+    document.querySelector("#progressBar").value = timeleft;
+    timeleft -= 1;
+
+  }
+}
+
+
+function random() {
+  if (level <= 5) {
+    let rl = Math.round(Math.random() * (threeLetters.length - 1));
+    rW = threeLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+
+  } else if ((level > 5) && (level <= 10)) {
+    let rl = Math.round(Math.random() * (fourLetters.length - 1));
+    rW = fourLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+
+  } else if ((level > 10) && (level <= 15)) {
+    let rl = Math.round(Math.random() * (fiveLetters.length - 1));
+    rW = fiveLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+  } else if ((level > 15) && (level <= 20)) {
+    let rl = Math.round(Math.random() * (sixLetters.length - 1));
+    rW = sixLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+
+  } else if ((level > 20) && (level <= 25)) {
+    let rl = Math.round(Math.random() * (sevenLetters.length - 1));
+    rW = sevenLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+  } else if ((level > 25) && (level <= 30)) {
+    let rl = Math.round(Math.random() * (eightLetters.length - 1));
+    rW = eightLetters[rl];
+    let b = document.querySelector('#randomWord');
+    b.innerHTML = rW;
+    return rW;
+  } else if (level > 30) {
+    console.log('hai vinto');
+  }
+
 }
